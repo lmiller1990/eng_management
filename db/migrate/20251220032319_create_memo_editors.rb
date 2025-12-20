6f199800a6746b5e1f87637bdbddb5b6f12eba43
Lachlan Miller <lachlan.miller.1990@outlook.com>
2025-12-20 13:28:21 +1000

    editors (#8)
    Co-authored-by: Lachlan Miller <lachlan.miller.199@outlook.com>

---
+++ db/migrate/20251220032319_create_memo_editors.rb
@@ -0,0 +1,12 @@
+class CreateMemoEditors < ActiveRecord::Migration[8.1]
+  def change
+    create_table :memo_editors do |t|
+      t.references :memo, null: false, foreign_key: true
+      t.references :account, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :memo_editors, [:memo_id, :account_id], unique: true
+  end
+end
