9eec7117d23c0931a7b5b1ce8d03e6faae2d8c71
Lachlan Miller <lachlan.miller.199@outlook.com>
2025-12-20 14:43:03 +1000

    chore: busted tests

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_20_032319) do
     t.index ["memo_id"], name: "index_memo_editors_on_memo_id"
   end
 
-<<<<<<< HEAD
   create_table "memo_invitations", force: :cascade do |t|
     t.datetime "accepted_at"
     t.datetime "created_at", null: false
@@ -99,8 +98,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_20_032319) do
     t.index ["token"], name: "index_memo_invitations_on_token", unique: true
   end
 
-=======
->>>>>>> origin/main
   create_table "memos", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.text "content"
@@ -168,11 +165,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_20_032319) do
   add_foreign_key "meeting_participants", "meetings"
   add_foreign_key "memo_editors", "accounts"
   add_foreign_key "memo_editors", "memos"
-<<<<<<< HEAD
   add_foreign_key "memo_invitations", "accounts", column: "inviter_id"
   add_foreign_key "memo_invitations", "memos"
-=======
->>>>>>> origin/main
   add_foreign_key "memos", "accounts"
   add_foreign_key "notes", "accounts"
   add_foreign_key "notes", "meetings"
