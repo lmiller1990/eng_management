9601723acd7d71ae6b0f3efbf9a6605d45767906
Lachlan Miller <lachlan.miller.1990@outlook.com>
2025-12-25 16:30:24 +1000

    UI tweaks

--- app/javascript/controllers/memos_controller.ts
+++ app/javascript/controllers/memos_controller.ts
@@ -6,6 +6,7 @@ export default class extends Controller {
 
   connect(): void {
     this.handleKeydown = this.handleKeydown.bind(this);
+    this.mouseenter = this.mouseenter.bind(this);
     document.addEventListener("keydown", this.handleKeydown);
     const $tbody = this.element.querySelector("tbody");
     if (!$tbody) {
@@ -17,18 +18,44 @@ export default class extends Controller {
     if (!this.memos.length) {
       return;
     }
+
     this.memos[this.index].classList.toggle("highlighted");
+
+    for (const memo of this.memos) {
+      memo.addEventListener("mouseenter", this.mouseenter);
+    }
   }
 
   disconnect() {
     document.removeEventListener("keydown", this.handleKeydown);
   }
 
-  private handleKeydown(event: KeyboardEvent) {
-    console.log(event.key);
+  mouseenter(event: MouseEvent) {
+    this.removeAllHighlighted();
 
+    if (!(event.currentTarget instanceof HTMLElement)) {
+      return;
+    }
+
+    const idx = event.currentTarget.dataset["index"];
+
+    if (!idx) {
+      throw new Error(`Should have data-index.`);
+    }
+
+    this.index = parseInt(idx, 10);
+    this.memos[this.index].classList.add("highlighted");
+    console.info(`Set index to ${this.index}`);
+  }
+
+  removeAllHighlighted() {
+    for (const memo of this.memos) {
+      memo.classList.remove("highlighted");
+    }
+  }
+
+  private handleKeydown(event: KeyboardEvent) {
     if (event.key === "Enter") {
-      console.log();
       const url = this.memos[this.index].dataset["url"];
       if (!url) {
         console.error(`Missing data-url on element: `, this.memos[this.index]);
